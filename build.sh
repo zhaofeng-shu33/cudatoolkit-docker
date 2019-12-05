7f0ee27d30572b0d98ee19352aadf7d6e2fe6d10
zhaofeng-shu33 <616545598@qq.com>
2019-12-05 15:00:00 +0800

    add basic compilers

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e -x
 echo "deb [ trusted=yes ] http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list
 apt update
-apt install --yes nvidia-cuda-toolkit
+apt install --yes cmake gcc g++ nvidia-cuda-toolkit
