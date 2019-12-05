f4f9f5ad450e6a254d3bf938e88d91c968ddcdad
zhaofeng-shu33 <616545598@qq.com>
2019-12-05 11:27:29 +0800

    fix build script not found

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu:16.04
 COPY build.sh /build_scripts/
-RUN bash /build/scripts/build.sh && rm -r /build_scripts
+RUN bash /build_scripts/build.sh && rm -r /build_scripts
 CMD ["/bin/bash"]
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -x
 echo "deb [ trusted=yes ] http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list
 apt update
 apt install nvidia-cuda-toolkit
