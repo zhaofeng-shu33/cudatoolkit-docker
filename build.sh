e6a3a62ee8e2f9df1cc97a0e990c3e851ba979af
zhaofeng-shu33 <616545598@qq.com>
2019-12-05 16:49:30 +0800

    install latest cmake using pip

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 set -e -x
 echo "deb [ trusted=yes ] http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list
 apt update
-apt install --yes cmake gcc g++ nvidia-cuda-toolkit
+apt install --yes python3-pip gcc g++ nvidia-cuda-toolkit
+pip3 install cmake
